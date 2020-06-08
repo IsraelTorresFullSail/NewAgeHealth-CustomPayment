03fc883dd3301fa6a1cb4b931313d1978ae3ddc5
Israel Torres <israeltorres@Mac-mini-69.local>
2020-06-08 10:16:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styles from './app.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import DirectDepositForm from './components/direct-deposit-form/direct-deposit-form';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className={styles.body}>
+          <main className={styles.main__container}>
+            <DirectDepositForm />
+          </main>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/direct-deposit-form/direct-deposit-form.jsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import Select from 'react-dropdown-select';
+
+import FormInput from '../form-input/form-input';
+import CustomButton from '../custom-button/custom-button';
+
+import './direct-deposit-form.scss';
+
+const optionsDay = [
+    { label: "1st", value: "1st" },
+    { label: "2nd", value: "2nd" },
+    { label: "3th", value: "3th" },
+];
+const optionsMonth = [
+    { label: "January", value: "January" },
+    { label: "February", value: "February" },
+    { label: "March", value: "March" },
+];
+const optionsTherapy = [
+    { label: "Hormone Replacement", value: "Hormone Replacement" },
+    { label: "Peptide Therapy", value: "Peptide Therapy" },
+    { label: "Vitamins", value: "Vitamins" },
+];
+
+class DirectDepositForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            amount: '',
+            day: '',
+            month: '',
+            therapy: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: '',
+            email: '',
+            phone: '',
+            selectedOptionDay: null,
+            selectedOptionMonth: null,
+            selectedOptionTherapy: null,
+        }
+    }
+
+    handleChangeDay = (selectedOptionDay) => {
+        this.setState({ selectedOptionDay });
+    }
+
+    handleChangeMonth = (selectedOptionMonth) => {
+        this.setState({ selectedOptionMonth });
+    }
+
+    handleChangeTherapy = (selectedOptionTherapy) => {
+        this.setState({ selectedOptionTherapy });
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+    };
+
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        const { name, amount, day, month, therapy, address, city, state, zip, email, phone } = this.state;
+
+        return(
+            <>
+                <h1>New Age Health And Wellness</h1>
+                <h2>12785 West Forest Hill Blvd, Wellington, Florida 33414</h2>
+                <h2>866-921-8779</h2>
+                <h1>Recurring Payment Authorization Form</h1>
+                <p>Schedule your payment to be automatically deducted from your bank account. Just complete and sign this form to get started!</p>
+                <h3>Here’s How Recurring Payments Work:</h3>
+                <p>You authorize regularly scheduled charges to your checking/savings account. You will be charged the amount indicated below each billing period. The charge will appear on your bank statement as an “ACH Debit.” You agree that no prior-notification will be provided unless the date or amount changes.</p>
+                <h3 className='red'>Address changes must be reported right away, if not and shipment goes out the patient is responsible for all loss of medications and shipping expenses.</h3>
+                <form method="POST" className='form' onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={this.handleChange}
+                        label="Full Name"
+                        required
+                    />
+                    <FormInput 
+                        type="number"
+                        name="amount"
+                        value={amount}
+                        onChange={this.handleChange}
+                        label="Amount"
+                        required
+                    />
+                    <Select 
+                        name="day"
+                        options={optionsDay}
+                        onChange={this.handleChangeDay}
+                        value={day}
+                        className='select'
+                    />
+                    <Select 
+                        name="month"
+                        options={optionsMonth}
+                        onChange={this.handleChangeMonth}
+                        value={month}
+                        className='select'
+                    />
+                    <Select 
+                        name="therapy"
+                        options={optionsTherapy}
+                        onChange={this.handleChangeTherapy}
+                        value={therapy}
+                        className='select'
+                    />
+                    <FormInput 
+                        type="text"
+                        name="address"
+                        value={address}
+                        onChange={this.handleChange}
+                        label="Street Address"
+                        required
+                    />
+                    <FormInput 
+                        type="text"
+                        name="city"
+                        value={city}
+                        onChange={this.handleChange}
+                        label="City"
+                        required
+                    />
+                    <FormInput 
+                        type="text"
+                        name="state"
+                        value={state}
+                        onChange={this.handleChange}
+                        label="State"
+                        required
+                    />
+                    <FormInput 
+                        type="number"
+                        name="zip"
+                        value={zip}
+                        onChange={this.handleChange}
+                        label="Postal / Zip Code"
+                        required
+                    />
+                    <FormInput 
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                        label="Email"
+                        required
+                    />
+                    <FormInput 
+                        type="number"
+                        name="phone"
+                        value={phone}
+                        onChange={this.handleChange}
+                        label="Phone"
+                        required
+                    />
+                    <CustomButton type="submit">Save</CustomButton>
+                </form>
+            </>
+        )
+    }
+}
+
+export default DirectDepositForm;
