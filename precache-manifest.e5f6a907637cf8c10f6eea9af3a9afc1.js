464fae05bb6e0d38af91ce6102decc9f0946a90d
Israel Torres <israeltorres@Israel-Mac-Mini.local>
2020-06-11 10:02:22 -0400

    Updates

---
+++ precache-manifest.e5f6a907637cf8c10f6eea9af3a9afc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625d97e8232b00caf2281f47ad202613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bc68f9a70c73aa2fe0f",
+    "url": "/static/css/main.67feec31.chunk.css"
+  },
+  {
+    "revision": "e2ec07eaee7c3b525f1f",
+    "url": "/static/js/2.49dfac65.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.49dfac65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc68f9a70c73aa2fe0f",
+    "url": "/static/js/main.41c7a697.chunk.js"
+  },
+  {
+    "revision": "5399181bf48f2256e3db",
+    "url": "/static/js/runtime-main.49be5b6a.js"
+  }
+]);
